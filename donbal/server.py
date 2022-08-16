920994adf19f2abc176dc1b6ac925b6a60503c7c
Parham Alvani <parham.alvani@gmail.com>
2022-08-17 02:55:22 +0430

    feat: new lines are important

--- donbal/server.py
+++ donbal/server.py
@@ -12,7 +12,10 @@ class Message:
 
 class MessageEncoder(json.JSONEncoder):
     def default(self, m: Message) -> dict[str, str]:
-        return {"line": m.line, "timestamp": m.timestamp.isoformat()}
+        return {
+            "line": m.line,
+            "timestamp": m.timestamp.isoformat(sep=" ", timespec="seconds"),
+        }
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -21,12 +24,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         conn, addr = s.accept()
         print(f"we have a new connection from {addr}")
+        print(json.dumps(Message("Hello World"), cls=MessageEncoder))
         conn.sendall(
             json.dumps(Message("Hello World"), cls=MessageEncoder).encode()
         )
+        conn.sendall(b"\n")
         time.sleep(1)  # wait for 1 sec
+        print(json.dumps(Message("I am Elahe Dastan"), cls=MessageEncoder))
         conn.sendall(
             json.dumps(
                 Message("I am Elahe Dastan"), cls=MessageEncoder
             ).encode()
         )
+        conn.sendall(b"\n")
