23b5ef91f95ced9e97498a1d0ede3c7ec577cbae
Parham Alvani <parham.alvani@gmail.com>
2022-08-17 01:59:16 +0430

    feat: add json messages

--- donbal/server.py
+++ donbal/server.py
@@ -1,5 +1,19 @@
 import socket
 import time
+import json
+import datetime
+
+
+class Message:
+    def __init__(self, line: str):
+        self.line = line
+        self.timestamp = datetime.datetime.now()
+
+
+class MessageEncoder(json.JSONEncoder):
+    def default(self, m: Message) -> dict[str, str]:
+        return {"line": m.line, "timestamp": m.timestamp.isoformat()}
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(("0.0.0.0", 1378))
@@ -7,6 +21,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         conn, addr = s.accept()
         print(f"we have a new connection from {addr}")
-        conn.sendall("Hello World\n".encode())
+        conn.sendall(
+            json.dumps(Message("Hello World"), cls=MessageEncoder).encode()
+        )
         time.sleep(1)  # wait for 1 sec
-        conn.sendall("I am Elahe Dastan\n".encode())
+        conn.sendall(
+            json.dumps(
+                Message("I am Elahe Dastan"), cls=MessageEncoder
+            ).encode()
+        )
